bd5e7719418efc65d79c593095a8f3c356152ea4
temito93 <76253491+temo93@users.noreply.github.com>
2021-05-31 18:28:02 +0400

    rewrited forms.js to native javascript

--- js/forms.js
+++ js/forms.js
@@ -1,14 +1,33 @@
-$(document).ready(function(){
-   $("#login").on("click",function(){
-      $("#popup").animate({"right":"0" , "opacity":"1"})
-   })
-   $("#close").on("click",function(){
-      $("#popup").animate({"right":"-50%", "opacity":"0"})
-   })
-   $("#popup").find("button").on("click",function(e){
-      e.preventDefault();
-   })
-})
+// $(document).ready(function(){
+//    $("#login").on("click",function(){
+//       $("#popup").animate({"right":"0" , "opacity":"1"})
+//    })
+//    $("#close").on("click",function(){
+//       $("#popup").animate({"right":"-50%", "opacity":"0"})
+//    })
+//    $("#popup").find("button").on("click",function(e){
+//       e.preventDefault();
+//    })
+// })
 
 
+const loginEl = document.querySelector('.login');
+const popupId = document.querySelector('#popup');
 
+loginEl.addEventListener('click', popupFunc);
+
+function popupFunc(event) {
+   event.preventDefault();
+   if(popupId.classList.contains('close')){
+      popupId.classList.remove('close');
+      popupId.classList.add('open');
+   } 
+   
+   //Close popup
+   const closeBtn = document.querySelector('.closeBtn');
+   closeBtn.addEventListener('click', function(){
+      popupId.classList.remove('open');
+      popupId.classList.add('close');
+   })
+   //---
+}
